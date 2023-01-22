d3be21b91c1689553b11fad91129a877dac2b783
Dino De La O <dinodelao@proton.me>
2023-01-21 19:37:39 -0600

    landing page mobile support

---
+++ src/components/ContextBox/index.js
@@ -0,0 +1,399 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+
+const learnContent = [
+    {
+      title: "Mars Hub",
+      description: "Deep dive into the Cosmos-SDK appchain that administers activity on Mars’ intergalactic outposts.",
+      listItems: [
+        {
+          href: "../../../../docs/learn/mars-hub/martian-council",
+          label: "Martian Council",
+        },
+  
+        {
+          href: "../../../../docs/learn/mars-hub/safety-fund",
+          label: "Safety Fund",
+        },
+
+        {
+          href: "../../../../docs/learn/mars-hub/tokenomics/tokenomics-intro",
+          label: "Tokenomics",
+        },
+      ],
+    },
+    {
+      title: "Red Bank",
+      description: "Enter the decentralized & fully automated, on-chain credit facility that can be deployed throughout the Cosmos.",
+      listItems: [
+        {
+          href: "../../../../docs/learn/red-bank/collateralised-borrowing",
+          label: "Collateralised Borrowing",
+        },
+  
+        {
+          href: "../../../../docs/learn/red-bank/contract-to-contract-lending",
+          label: "Contract-to-Contract Lending",
+        },
+
+      ],
+    },
+    {
+      title: "Rover",
+      description: "Abandon centralized exchanges and ride Mars’ new generalised credit primitive called Rover.",
+      listItems: [
+        {
+          href: "../../../../docs/learn/rover/nft-credit-accounts",
+          label: "NFT Credit Accounts",
+        },
+  
+        {
+          href: "../../../../docs/learn/rover/trading/trading-intro",
+          label: "Trading",
+        },
+
+      ],
+    },
+
+    {
+      title: "Vaults",
+      description: "Learn how Mars implements automated strategies in a firewalled container.",
+      listItems: [
+        {
+          href: "../../../../docs/learn/vaults/fields-of-mars",
+          label: "Fields of Mars",
+        },
+
+      ],
+    },
+
+    {
+      title: "Workstation",
+      description: "Tutorials and beginner guides to Mars Protocol with Martian-approved step-by-step screenshots.",
+      listItems: [
+        {
+          href: "../../../../docs/learn/workstation/basics/basics-intro",
+          label: "Basics",
+        },
+
+        {
+          href: "../../../../docs/learn/workstation/airdrop",
+          label: "Airdrop",
+        },
+
+        {
+          href: "../../../../docs/learn/workstation/red-bank/red-bank-intro",
+          label: "Red Bank",
+        },
+
+      ],
+    },
+
+  ]
+  
+  const governContent = [
+    {
+      title: "Martian Council",
+      description: "Mars Protocol’s governing body.",
+      listItems: [
+        {
+          href: "../../../../docs/govern/martian-council",
+          label: "Overview",
+        },
+
+      ],
+    },
+
+    {
+      title: "Mars Improvement Proposals",
+      description: "The blueprint that empowers Council participants to submit and approve on-chain governance proposals.",
+      listItems: [
+        {
+          href: "../../../../docs/govern/mars-improvement-proposals#templates",
+          label: "Templates",
+        },
+
+        {
+          href: "../../../../docs/govern/mars-improvement-proposals#proposing",
+          label: "Proposing",
+        },
+
+      ],
+    },
+
+    {
+      title: "Mars Risk Framework",
+      description: "A framework for determining the risk parameters associated with assets and credit lines on Mars.",
+      listItems: [
+        {
+          href: "../../../../docs/govern/mars-risk-framework#framework-summary",
+          label: "Framework Summary",
+        },
+
+        {
+          href: "../../../../docs/govern/mars-risk-framework#supporting-new-assets-and-lp-tokens",
+          label: "Supporting New Assets and LP Tokens",
+        },
+
+      ],
+    },
+
+  ];
+  
+  const developContent = [
+    {
+      title: "Mars CLI",
+      description: "Interact with Mars' function via the terminal.",
+      listItems: [
+        {
+          href: "../../../../docs/develop/mars-cli/public-endpoints",
+          label: "Public Endpoints",
+        },
+  
+        {
+          href: "../../../../docs/develop/mars-cli/marsd",
+          label: "Marsd",
+        },
+  
+        {
+          href: "../../../../docs/develop/mars-cli/generating-signing-and-broadcasting-transactions",
+          label: "Broadcasting Transactions",
+        },
+      ],
+    },
+    {
+      title: "Contracts",
+      description: "Overview of all Martian smart contracts.",
+      listItems: [
+        {
+          href: "../../../../docs/develop/contracts/mars-hub/mars-hub-intro",
+          label: "Mars Hub",
+        },
+  
+        {
+          href: "../../../../docs/develop/contracts/outposts/outposts-intro",
+          label: "Outposts",
+        },
+
+      ],
+    },
+    {
+      title: "Modules",
+      description: "Enter the realm of CosmosSDK, CosmWasm, and IBC modules.",
+      listItems: [
+        {
+          href: "../../../../docs/develop/modules/module-accounts",
+          label: "Module Accounts",
+        },
+  
+        {
+          href: "../../../../docs/develop/modules/custom-modules/custom-modules-intro",
+          label: "Custom Modules",
+        },
+  
+        {
+          href: "../../../../docs/develop/modules/cosmos-sdk-modules/cosmos-sdk-modules-intro",
+          label: "Cosmos SDK Modules",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      description: "Insights and details on the red backend.",
+      listItems: [
+        {
+          label: "Liquidation Bots",
+          href: "../../../../docs/develop/category/liquidation-bots",
+        },
+      ],
+    },
+  ];
+
+  
+  const validateContent = [
+    {
+      title: "Node Operators",
+      description: "Set up a Martian node.",
+      listItems: [
+        {
+          href: "../../../../docs/validate/node-operators/setting-up-a-node",
+          label: "Setting up a Node",
+        },
+  
+        {
+          href: "../../../../docs/validate/node-operators/interacting-with-your-node",
+          label: "Interacting with your Node",
+        },
+  
+      ],
+    },
+    {
+      title: "Validators",
+      description: "Spin up a Martian validator.",
+      listItems: [
+        {
+          href: "../../../../docs/validate/validators/becoming-a-validator",
+          label: "Becoming a Validator",
+        },
+  
+        {
+          href: "../../../../docs/validate/validators/managing-your-validator",
+          label: "Managing your Validator",
+        },
+
+        {
+          href: "../../../../docs/validate/validators/validators-faq",
+          label: "Validators FAQ",
+        },
+  
+      ],
+    },
+    {
+      title: "IBC Relayers",
+      description: "Get started with IBC Relayers.",
+      listItems: [
+        {
+          href: "../../../../docs/validate/ibc-relayers/channels-for-mars",
+          label: "Channels for Mars",
+        },
+  
+        {
+          href: "../../../../docs/validate/ibc-relayers/relayers-guide",
+          label: "Relayers Guide",
+        },
+  
+      ],
+    },
+  ];
+  
+
+const Card = ({ children }) => <div className="max-w-[550px] w-full h-[250px]">{children}</div>;
+
+export default function ContentBox(props) {
+    if (props.title == "Learn") {
+      return (
+        <div className="flex flex-wrap justify-center gap-4 mb-20">
+          {learnContent.map((entry) => (
+            <Card key={entry.title}>
+              <a className={styles.links} href="../../../../docs/learn/overview">
+                <div className={styles.box}>
+                  {/* <img src={entry.image} height={76} width={76} className="opacity-50 self-start mt-4" /> */}
+                  <div className={styles.titleBanner}>
+                      <p className={styles.title}>{entry.title}</p>
+                  </div>
+
+                  <div>
+                    {/* <p className={styles.title}>{entry.title}</p> */}
+                    <p className={styles.subtext}>{entry.description}</p>
+                    <ul className={styles.list}>
+                      {entry.listItems.map((item) => (
+                        <li key={item.label}>
+                          <a className={styles.links} href={item.href}>
+                            {item.label}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </a>
+            </Card>
+          ))}
+        </div>
+      );
+    } else if (props.title == "Govern") {
+      return (
+        <div className="flex flex-wrap justify-center gap-4 mb-20">
+          {governContent.map((entry) => (
+            <Card key={entry.title}>
+              <a className={styles.links} href="../../../../docs/govern/overview">
+                <div className={styles.box}>
+                  {/* <img src={entry.image} height={76} width={76} className="opacity-50 self-start mt-4" /> */}
+                  <div className={styles.titleBanner}>
+                      <p className={styles.title}>{entry.title}</p>
+                  </div>
+                  
+                  <div>
+                    {/* <p className={styles.title}>{entry.title}</p> */}
+                    <p className={styles.subtext}>{entry.description}</p>
+                    <ul className={styles.list}>
+                      {entry.listItems.map((item) => (
+                        <li key={item.label}>
+                          <a className={styles.links} href={item.href}>
+                            {item.label}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </a>
+            </Card>
+          ))}
+        </div>
+      );
+    } else if (props.title == "Develop") {
+      return (
+        <div className="flex flex-wrap justify-center gap-4 mb-20">
+          {developContent.map((entry) => (
+            <Card key={entry.title}>
+              <a className={styles.links} href="../../../../docs/develop/overview">
+                <div className={styles.box}>
+                  {/* <img src={entry.image} height={76} width={76} className="opacity-50 self-start mt-4" /> */}
+                  <div className={styles.titleBanner}>
+                      <p className={styles.title}>{entry.title}</p>
+                  </div>
+
+                  <div>
+                    {/* <p className={styles.title}>{entry.title}</p> */}
+                    <p className={styles.subtext}>{entry.description}</p>
+                    <ul className={styles.list}>
+                      {entry.listItems.map((item) => (
+                        <li key={item.label}>
+                          <a className={styles.links} href={item.href}>
+                            {item.label}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </a>
+            </Card>
+          ))}
+        </div>
+      );
+    } else if (props.title == "Validate") {
+      return (
+        <div className="flex flex-wrap justify-center gap-4 mb-20">
+          {validateContent.map((entry) => (
+            <Card key={entry.title}>
+              <a className={styles.links} href="../../../../docs/validate/overview">
+                <div className={styles.box}>
+                  {/* <img src={entry.image} height={76} width={76} className="opacity-50 self-start mt-4" /> */}
+                  <div className={styles.titleBanner}>
+                      <p className={styles.title}>{entry.title}</p>
+                  </div>
+                  
+                  <div>
+                    {/* <p className={styles.title}>{entry.title}</p> */}
+                    <p className={styles.subtext}>{entry.description}</p>
+                    <ul className={styles.list}>
+                      {entry.listItems.map((item) => (
+                        <li key={item.label}>
+                          <a className={styles.links} href={item.href}>
+                            {item.label}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </a>
+            </Card>
+          ))}
+        </div>
+      );
+    }
+  }
--- src/pages/index.js
+++ src/pages/index.js
@@ -22,13 +22,19 @@ import GovernFigUrl from '@site/static/img/icons/balance.png';
 import DevelopFigUrl from '@site/static/img/icons/pioneer.png';
 import InfrastructureFigUrl from '@site/static/img/icons/satellite.png';
 
+import ContentBox from "../components/ContextBox";
+
+const subheaderClasses = "opacity-60 md:px-[25%] mb-10 text-lg text-center";
+
+
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+
+    <header className={styles.heroBanner}>
+      <div className={styles.bannerText}>
         {/* <div className={styles.bannerFig}>
           <img src={BannerFigUrl} />
         </div>  */}
@@ -37,10 +43,31 @@ function HomepageHeader() {
         <p className={styles.subtext}>Discover the autonomous credit protocol of the Cosmos universe</p>
         {/* <p>You've arrived at the Mars Codex, the central information station for Mars Protocol.   or discover more below </p> <a href="../docs/overview/intro-to-mars-protocol/intro-to-mars-protocol-intro">Read more about MARS</a> */}
 
-        
-        <div className={clsx(styles.arrows)}></div>
+
+        <div className={styles.arrows}></div>
+
       </div>
+
     </header>
+
+
+
+    // <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    //   <div className="container">
+    //     {/* <div className={styles.bannerFig}>
+    //       <img src={BannerFigUrl} />
+    //     </div>  */}
+    //     <h1 className={styles.hero__title}>Mars Protocol Docs</h1>
+    //     {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
+    //     <p className={styles.subtext}>Discover the autonomous credit protocol of the Cosmos universe</p>
+    //     {/* <p>You've arrived at the Mars Codex, the central information station for Mars Protocol.   or discover more below </p> <a href="../docs/overview/intro-to-mars-protocol/intro-to-mars-protocol-intro">Read more about MARS</a> */}
+
+
+    //     {/* <div className={clsx(styles.arrows)}></div> */}
+
+
+    //   </div>
+    // </header>
   );
 }
 
@@ -101,9 +128,66 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       
+      <main className="z-10 lg:py-12 overflow-x-hidden">
         <HomepageHeader />
 
-        <div className={styles.ContentBlocks}> 
+        {/* flex flex-col gap-y-10 px-4 w-full mx-auto */}
+        <div className={styles.ContentBlocks}>
+          <div>
+
+            <div className={styles.figs}>
+              <img src={LearnFigUrl} />
+            </div>
+
+            <h2 className={styles.title}>Learn</h2>
+            <p className={subheaderClasses}>
+            Explore the outposts of Mars. Discover ways to deepen your understanding, whether you are a DeFi enthusiast, a validator, or a seasoned trader looking to expand your proficiency. 
+            </p>
+            <ContentBox title="Learn" />
+          </div>
+
+          <div>
+
+            <div className={styles.figs}>
+              <img src={GovernFigUrl} />
+            </div>
+
+            <h2 className={styles.title}>Govern</h2>
+            <p className={subheaderClasses}>
+            Discover how to become an active community member of the Mars Protocol governing body, the Martian Council, and help steer the future of Mars.
+            </p>
+            <ContentBox title="Govern" />
+          </div>
+
+          <div>
+
+            <div className={styles.figs}>
+              <img src={DevelopFigUrl} />
+            </div>
+
+            <h2 className={styles.title}>Develop</h2>
+            <p className={subheaderClasses}>
+            Expand your developer skills and explore the exciting realm of Mars Protocol's open-sourced code base.
+            </p>
+            <ContentBox title="Develop" />
+          </div>
+
+          <div>
+
+            <div className={styles.figs}>
+              <img src={InfrastructureFigUrl} />
+            </div>
+
+            <h2 className={styles.title}>Validate</h2>
+            <p className={subheaderClasses}>
+            Learn how to become a node operator, relayer, or validator on Mars Hub.
+            </p>
+            <ContentBox title="Validate" />
+          </div>
+
+        </div>
+
+        {/* <div className={styles.ContentBlocks}> 
 
         <ContentBlocks 
           banner={contentBlockProps.learn.banner}
@@ -137,9 +221,9 @@ export default function Home() {
           footer={contentBlockProps.infrastructure.footer}
         />
       
-      </div>
+      </div> */}
 
-      <main>
+
       </main>
 
     </Layout>
