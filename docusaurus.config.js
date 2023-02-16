1de292894710ef6db93a1a19fb4cdab277e98c81
Dino De La O <dinodelao@proton.me>
2023-02-16 01:00:23 -0600

    algolia search bar integration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -211,6 +211,37 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+
+      algolia: {
+      // The application ID provided by Algolia
+      appId: 'PKVTT851E2',
+
+      // Public API key: it is safe to commit it
+      apiKey: '8390d92a26d41f3069f2a03dc8074d47',
+
+      indexName: 'marsprotocol',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      //... other Algolia params
+      },
+
       navbar: {
         title: 'Home',
         // logo: {
